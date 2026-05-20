f7ab9d20a5b0eb0c5f1aa40e93967c1d1fbae836
Udochukwu Agwulonu <udochukwuagwulonu@gmail.com>
2026-05-20 14:42:23 +0100

    Fix providers conflict: restructure auth config for Edge Runtime

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,5 @@
 import bcrypt from "bcryptjs";
 import NextAuth from "next-auth";
-import authConfig from "./auth.config";
 import Credentials from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "./lib/db";
@@ -59,5 +58,4 @@ export const {
       }
     })
   ],
-  ...authConfig,
 });
